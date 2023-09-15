86dfce6456be01a5a3c9220a22cfacbe328efa13
akshay-kumar-singh <akshaysing975@gmail.com>
2023-09-15 20:19:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskList from './components/TaskList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
